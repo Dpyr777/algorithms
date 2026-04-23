8bd77f894c31179c19c90a1b530ee8625bcd19f6
Melesh Andrey <dpyr-a@yandex.ru>
2026-04-24 01:19:44 +0300

    major progress on grad_descent + add random helper function

--- grad_descent.c
+++ grad_descent.c
@@ -1,28 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <time.h>
 #include "derivative.h"
+#include "grad_descent.h"
 
+double get_random_start_point();
 
-void grad_desent(char* function){
+void grad_descent(char* function){
 #if 0
     char* derX = derivative(function, 'x');
     char* derY = derivative(function, 'y');
 #endif
     srand(time(NULL));
-    printf("derX = 2 * x + 2 - 400 * x * y + 400 * x^3");
-    printf("derY = 2 * y - 200 * x^2");
+    printf("derX = 2 * x - 2 - 400 * x * y + 400 * x^3\n");
+    printf("derY = 200 * y - 200 * x^2\n");
 
     
-    float x0 = 1.0f / (rand() % 10); 
-    if ((rand() & 1) == 0) {
-        x0 = -x0;
-    }
-    printf("x0 = %f", x0);
+    double xNew;
+    double yNew;
 
-    float y0 = 1.0f / (rand() % 10); 
-    if ((rand() & 1) == 0) {
-        y0 = -y0;
+    double xOld;
+    double yOld;
+
+    double f; 
+
+    double step = 0.001f;
+    double eps = 0.00001f;
+    for (int i = 0; i < 10; ++i){
+        xNew = get_random_start_point(); 
+        printf("x0 = %f\n", xNew);
+
+        yNew = get_random_start_point();
+        printf("y0 = %f\n", yNew);
+
+        xOld = 0.0;
+        yOld = 0.0;
+
+        f = (1 - xNew) * (1 - xNew) + 100 * (yNew - xNew * xNew) * (yNew - xNew * xNew);
+        printf("minum function in points x = %.2f; y = %.2f  => f = %.2f\n", xNew, yNew, f);
+
+        while (fabs(xNew - xOld) > eps || fabs(yNew - yOld) > eps){
+            xOld = xNew;
+            yOld= yNew;
+            xNew = xOld - step * (2 * xOld - 2 - 400 * xOld * yOld + 400 * powf(xOld, 3));  
+            yNew = yOld - step * (200 * yOld - 200 * powf(xOld, 2));
+            f = (1 - xNew) * (1 - xNew) + 100 * (yNew - xNew * xNew) * (yNew - xNew * xNew);
+            //printf("minum function in points x = %.2f; y = %.2f  => f = %.8f\n", xNew, yNew, f);
+            if (fabs(xNew / xOld) > 5.0 || fabs(yNew / xOld) > 5.0){
+                printf("Blow-up\n");
+                break;
+            }
+        }
+        printf("minum function in points x = %.2f; y = %.2f  => f = %.4f\n", xNew, yNew, f);
     }
-    printf("y0 = %f", y0);
 }
+double get_random_start_point(){
+    double point = 1.0f / (rand() % 10 + 1); 
+    if ((rand() & 1) == 0) {
+        point = -point;
+    }
+    return point; 
+}
+
--- main.c
+++ main.c
@@ -3,13 +3,12 @@
 
 int main(){
 
-
 #if 0
     char* function = (char*)malloc(100 * size(char));
     scanf("%99s", function);
 #endif
     char* function = "(1 - x)^2 + 100 * (y - x^2)^2";
-    printf("Function Rosenbrock -> f(x, y) = %s", function); 
+    printf("Function Rosenbrock -> f(x, y) = %s\n", function); 
     grad_descent(function);
     
     return 0;
